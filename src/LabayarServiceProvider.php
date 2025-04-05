2cf3fb7e46a3292ab6c0ec285a8a797ddf9c37fd
masraga <github.ragamulia@gmail.com>
2025-04-05 23:26:56 +0700

    adding project service provider

--- src/LabayarServiceProvider.php
+++ src/LabayarServiceProvider.php
@@ -1,15 +1,9 @@
 <?php
-namespace Koderpedia\Labayar;
 
 use Illuminate\Support\ServiceProvider;
 
-class LabayarServiceProvider extends ServiceProvider {
-  public function boot(){
-
-  }
-
-  public function register()
-  {
-    
-  }
+class LabayarServiceProvider extends ServiceProvider
+{
+  public function boot() {}
+  public function register() {}
 }
